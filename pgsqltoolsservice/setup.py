774f7ae5251f655bfeadd5be595ba6353e1bb4d6
Matt Irvine <mairvine@microsoft.com>
2017-05-17 16:17:01 -0700

    Support building executables using cx_freeze

---
+++ pgsqltoolsservice/setup.py
@@ -0,0 +1,17 @@
+from cx_Freeze import setup, Executable
+
+# Dependencies are automatically detected, but it might need
+# fine tuning.
+buildOptions = dict(packages = [], excludes = [])
+
+base = 'Console'
+
+executables = [
+    Executable('server.py', base=base)
+]
+
+setup(name='PostgreSQL Tools Service',
+      version = '0.1.0',
+      description = 'VSCode language server implementation for PostgreSQL',
+      options = dict(build_exe = buildOptions),
+      executables = executables)
