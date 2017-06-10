87f059c1dd9ab53caa2809c057a37682cf47cd56
Henry Phan <huypha@microsoft.com>
2017-06-09 17:01:45 -0700

    Added None check in utils time functions

--- pgsqltoolsservice/utils/time.py
+++ pgsqltoolsservice/utils/time.py
@@ -9,10 +9,14 @@ from datetime import datetime #noaq
 
 def get_time_str(time: datetime):
     """Convert a time object into a standard user-readable string"""
+    if time is None:
+        return None
     return time.strftime('%H:%M:%S.%f')
 
 
 def get_elapsed_time_str(start_time: datetime, end_time: datetime):
     """Get time difference between two times as a user-readable string"""
+    if start_time is None or end_time is None:
+        return None
     elapsed_time = end_time - start_time
     return str(elapsed_time)
