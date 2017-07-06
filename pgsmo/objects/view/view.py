c5ee3637ad8a5d282ab0e33a7d6c0719ffc27d3c
Benjamin Russell <beruss@microsoft.com>
2017-07-06 11:49:43 -0700

    Adding tests for view class

--- pgsmo/objects/view/view.py
+++ pgsmo/objects/view/view.py
@@ -8,19 +8,20 @@ from typing import List
 
 from pgsmo.objects.column.column import Column
 import pgsmo.objects.node_object as node
-import pgsmo.utils as utils
+import pgsmo.utils.querying as querying
+import pgsmo.utils.templating as templating
 
-TEMPLATE_ROOT = utils.templating.get_template_root(__file__, 'view_templates')
+TEMPLATE_ROOT = templating.get_template_root(__file__, 'view_templates')
 
 
 class View(node.NodeObject):
     @classmethod
-    def get_nodes_for_parent(cls, conn: utils.querying.ServerConnection, scid: int) -> List['View']:
+    def get_nodes_for_parent(cls, conn: querying.ServerConnection, scid: int) -> List['View']:
         type_template_root = path.join(TEMPLATE_ROOT, conn.server_type)
         return node.get_nodes(conn, type_template_root, cls._from_node_query, scid=scid)
 
     @classmethod
-    def _from_node_query(cls, conn: utils.querying.ServerConnection, **kwargs) -> 'View':
+    def _from_node_query(cls, conn: querying.ServerConnection, **kwargs) -> 'View':
         """
         Creates a view object from the results of a node query
         :param conn: Connection used to execute the nodes query
@@ -35,7 +36,7 @@ class View(node.NodeObject):
 
         return view
 
-    def __init__(self, conn: utils.querying.ServerConnection, name: str):
+    def __init__(self, conn: querying.ServerConnection, name: str):
         super(View, self).__init__(conn, name)
 
         # Declare child items
