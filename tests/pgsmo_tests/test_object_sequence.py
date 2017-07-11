0744f8d591317a71261af952bdfcbd825deb86a3
Benjamin Russell <beruss@microsoft.com>
2017-07-11 16:23:51 -0700

    Cleaning up unit test

--- tests/pgsmo_tests/test_object_sequence.py
+++ tests/pgsmo_tests/test_object_sequence.py
@@ -33,9 +33,4 @@ class TestFunction(unittest.TestCase):
 
     # IMPLEMENTATION DETAILS ###############################################
     def _validate(self, obj: Sequence, mock_conn: ServerConnection):
-        # NodeObject basic properties
-        self.assertIs(obj._conn, mock_conn)
-        self.assertEqual(obj._oid, NODE_ROW['oid'])
-        self.assertEqual(obj.oid, NODE_ROW['oid'])
-        self.assertEqual(obj._name, NODE_ROW['name'])
-        self.assertEqual(obj.name, NODE_ROW['name'])
+        utils.validate_node_object_props(obj, mock_conn, NODE_ROW['name'], NODE_ROW['oid'])
