2c0a20a32f991405e3565b464765b1df974e45e2
Benjamin Russell <beruss@microsoft.com>
2017-07-11 15:34:25 -0700

    Adding docstrings for Constraint class

--- pgsmo/objects/table_objects/constraints.py
+++ pgsmo/objects/table_objects/constraints.py
@@ -12,12 +12,30 @@ import pgsmo.utils.templating as templating
 
 
 class Constraint(node.NodeObject, metaclass=ABCMeta):
+    """Base class for constraints. Provides basic properties for all constraints"""
+
     @classmethod
     def get_nodes_for_parent(cls, conn: querying.ServerConnection, tid: int) -> List['Constraint']:
+        """
+        Generates a list of constraints by executing nodes.sql
+        :param conn: The connection to use to execute the nodes query
+        :param tid: ID of the table that owns the constraints
+        :return: A list of constraint objects (can be any of the Constraint subclasses)
+        """
         return node.get_nodes(conn, cls._template_path(), cls._from_node_query, tid=tid)
 
     @classmethod
     def _from_node_query(cls, conn: querying.ServerConnection, **kwargs) -> 'Constraint':
+        """
+        Creates a constraint from the results of a node query for any constraint
+        :param conn: The connection used to execute the nodes query
+        :param kwargs: A row from a constraint nodes query
+        Kwargs:
+            name str: Name of the constraint
+            oid int: Object ID of the constraint
+            convalidated bool: ? TODO: Figure out what this value means
+        :return: An instance of a constraint
+        """
         constraint = cls(conn, kwargs['name'])
         constraint._oid = kwargs['oid']
         constraint._convalidated = kwargs['convalidated']
