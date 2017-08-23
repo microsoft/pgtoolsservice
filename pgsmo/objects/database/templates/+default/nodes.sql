8182e842e6474df8845f438ad0fdbc105a2aa2cd
Ravindra Kasireddy <rkasi@microsoft.com>
2017-08-22 22:46:02 -0700

    DB support

--- pgsmo/objects/database/templates/+default/nodes.sql
+++ pgsmo/objects/database/templates/+default/nodes.sql
@@ -6,7 +6,7 @@
  #}
 SELECT
     db.oid as oid, db.datname as name, ta.spcname as spcname, db.datallowconn,
-    has_database_privilege(db.oid, 'CREATE') as cancreate, datdba as owner
+    has_database_privilege(db.oid, 'CREATE') as cancreate, datdba as owner, db.datistemplate
 FROM
     pg_database db
     LEFT OUTER JOIN pg_tablespace ta ON db.dattablespace = ta.oid
