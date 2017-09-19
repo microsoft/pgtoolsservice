6505a624663c5b746f60759cd4a2ec3174a1bfc1
Matt Irvine <mji7wb@virginia.edu>
2017-09-19 16:27:08 -0700

    Add integration test support (#135)

---
+++ scripts/test-unit.ps1
@@ -0,0 +1,8 @@
+# Save the current directory and the script's directory, since tests must be run from the project root
+$curloc = $pwd
+$scriptloc = $PSScriptRoot
+Set-Location $scriptloc/..
+
+# Run the tests
+nosetests -a '!is_integration_test' @args
+Set-Location $curloc
