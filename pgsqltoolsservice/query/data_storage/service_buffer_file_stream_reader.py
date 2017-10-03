29d7a27869f691e07b8f4fa73ac8867e7315ae59
liusun <liusun@microsoft.com>
2017-10-03 11:02:55 -0700

    adding reader files

---
+++ pgsqltoolsservice/query/data_storage/service_buffer_file_stream_reader.py
@@ -0,0 +1,38 @@
+# --------------------------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for license information.
+# --------------------------------------------------------------------------------------------
+
+import io
+
+
+class ServiceBufferFileStreamReader:
+    """ Writer for service buffer formatted file streams """
+
+    READER_STREAM_NONE_ERROR = "Stream argument is None"
+    READER_STREAM_NOT_SUPPORT_WRITING_ERROR = "Stream argument doesn't support reading"
+    READER_DATA_READ_ERROR = "Data read error"
+    ##CONVERTER_DATA_TYPE_NOT_EXIST_ERROR = "Convert to bytes not supported"   
+
+    def __init__(self, stream: io.BytesIO) -> None:
+
+        if stream is None:
+            raise ValueError(ServiceBufferFileStreamReader.READER_STREAM_NONE_ERROR)
+
+        if not stream.readable():
+            raise ValueError(ServiceBufferFileStreamReader.READER_STREAM_NOT_SUPPORT_WRITING_ERROR)
+
+        self._file_stream = stream
+
+        self._close_stream_flag = False
+
+    def _write_null(self):
+        val_byte_array = bytearray([])
+        return self._write_to_file(self._file_stream, val_byte_array)
+
+    def _write_to_file(self, stream, byte_array):
+        try:
+            written_byte_number = stream.write(byte_array)
+        except Exception as exc:
+            raise IOError(ServiceBufferFileStreamReader.WRITER_DATA_WRITE_ERROR) from exc
+        return written_byte_number
