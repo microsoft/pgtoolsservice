7a2e019f5ae73ba406c9f8dd53db4766a0155fef
liusun <liusun@microsoft.com>
2017-10-05 02:23:34 -0700

    init for buffer reader

--- pgsqltoolsservice/query/data_storage/service_buffer_file_stream_reader.py
+++ pgsqltoolsservice/query/data_storage/service_buffer_file_stream_reader.py
@@ -4,35 +4,91 @@
 # --------------------------------------------------------------------------------------------
 
 import io
+from typing import List
+import struct
+
+from pgsqltoolsservice.parsers import datatypes
+from pgsqltoolsservice.query_execution.contracts.common import DbCellValue
+from pgsqltoolsservice.query_execution.contracts.common import DbColumn
+from pgsqltoolsservice.query.data_storage.converters.objects_converter import get_objects_converter
 
 
 class ServiceBufferFileStreamReader:
-    """ Writer for service buffer formatted file streams """
+    """ Reader for service buffer formatted file streams """
 
     READER_STREAM_NONE_ERROR = "Stream argument is None"
-    READER_STREAM_NOT_SUPPORT_WRITING_ERROR = "Stream argument doesn't support reading"
+    READER_STREAM_NOT_SUPPORT_READING_ERROR = "Stream argument doesn't support reading"
     READER_DATA_READ_ERROR = "Data read error"
-    ##CONVERTER_DATA_TYPE_NOT_EXIST_ERROR = "Convert to bytes not supported"   
 
-    def __init__(self, stream: io.BytesIO) -> None:
+    def __init__(self, stream: io.BufferedReader) -> None:
 
         if stream is None:
             raise ValueError(ServiceBufferFileStreamReader.READER_STREAM_NONE_ERROR)
 
         if not stream.readable():
-            raise ValueError(ServiceBufferFileStreamReader.READER_STREAM_NOT_SUPPORT_WRITING_ERROR)
+            raise ValueError(ServiceBufferFileStreamReader.READER_STREAM_NOT_SUPPORT_READING_ERROR)
 
         self._file_stream = stream
 
         self._close_stream_flag = False
 
-    def _write_null(self):
-        val_byte_array = bytearray([])
-        return self._write_to_file(self._file_stream, val_byte_array)
-
-    def _write_to_file(self, stream, byte_array):
+    def _read_bytes_from_file(self, stream, file_offset, length_to_read) -> bytes:
         try:
-            written_byte_number = stream.write(byte_array)
+            # locate the offset position before read
+            stream.seek(file_offset)
+            # read the bytes data based on the length_to_read
+            read_bytes_result = stream.read(length_to_read)
         except Exception as exc:
-            raise IOError(ServiceBufferFileStreamReader.WRITER_DATA_WRITE_ERROR) from exc
-        return written_byte_number
+            raise IOError(ServiceBufferFileStreamReader.READER_DATA_READ_ERROR) from exc
+            stream.close()
+        return read_bytes_result
+
+    def read_row(self, file_offset, row_id, columns_info: List[DbColumn]) -> List[DbCellValue]:
+        """   Read a row from a file   """
+        self._file_stream.seek(file_offset)
+
+        len_columns_info = len(columns_info)
+        current_file_offset = file_offset
+        results = []  # list of DbCellValue as return
+
+        for index in range(0, len_columns_info):
+            self._close_stream_flag = (index == len_columns_info - 1)
+
+            type_value = columns_info[index].data_type_name
+
+            # Read the object from the temp file
+            if type_value == datatypes.DATATYPE_NULL:
+                # wrap the NULL value as a DbCellValue
+                display_value = "NULL"
+                is_null = True
+                raw_object = "NULL"
+                row_id = row_id
+                value = DbCellValue(display_value, is_null, raw_object, row_id)
+            else:
+                # read the length of data, then update the offset by plus 4, since the int hold 4 bytes
+                raw_bytes_length_to_read = self._read_bytes_from_file(self._file_stream, current_file_offset, 4)
+                bytes_length_to_read = struct.unpack('i', raw_bytes_length_to_read)[0]
+                current_file_offset += 4
+
+                # read the data content based on the length of data
+                read_bytes_result = self._read_bytes_from_file(self._file_stream, current_file_offset, bytes_length_to_read)
+                read_bytes_length = len(read_bytes_result)
+                current_file_offset += read_bytes_length
+
+                # convert data_bytes to data_obj
+                object_converter: Callable[[bytes], Any] = get_objects_converter(type_value)
+                result_object = object_converter(read_bytes_result)
+
+                # wrap the result_object as a DbCellValue
+                display_value = str(result_object)
+                is_null = False
+                raw_object = result_object
+                row_id = row_id
+                value = DbCellValue(display_value, is_null, raw_object, row_id)
+
+            results.append(value)
+
+            if self._close_stream_flag:
+                self._file_stream.close()
+
+        return results
---
+++ pgsqltoolsservice/query/data_storage/test_service_buffer_file_stream_reader.py
@@ -0,0 +1,112 @@
+# --------------------------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for license information.
+# --------------------------------------------------------------------------------------------
+
+import unittest
+import struct
+
+from pgsqltoolsservice.query.data_storage.service_buffer_file_stream_reader import ServiceBufferFileStreamReader
+from pgsqltoolsservice.query_execution.contracts.common import DbColumn
+from pgsqltoolsservice.parsers import datatypes
+
+
+class TestServiceBufferFileStreamReader(unittest.TestCase):
+
+    def setUp(self):
+
+        # test data
+        self._bool_test_value = True
+        self._float_test_value = 123.45600128173828
+        self._int_test_value = 123456
+
+        # file_streams:
+        self._bool_file_stream = open("E:\\temp.txt", "wb")
+        bool_val = bytearray(struct.pack("?", self._bool_test_value))
+        bool_len = len(bool_val)
+        bool_len_to_write = bytearray(struct.pack("i", bool_len))
+        self._bool_file_stream.write(bool_len_to_write)
+        self._bool_file_stream.write(bool_val)
+        self._bool_file_stream.close()
+        self._bool_file_stream = open("E:\\temp.txt", "rb")
+
+        self._float_file_stream = open("E:\\temp.txt", "wb")
+        float_val = bytearray(struct.pack("f", self._float_test_value))
+        float_len = len(float_val)
+        float_len_to_write = bytearray(struct.pack("i", float_len))
+        self._float_file_stream.write(float_len_to_write)
+        self._float_file_stream.write(float_val)
+        self._float_file_stream.close()
+        self._float_file_stream = open("E:\\temp.txt", "rb")
+
+        self._multiple_cols_file_stream = open("E:\\temp.txt", "wb")
+        val0 = bytearray(struct.pack("f", self._float_test_value))
+        len0 = len(val0)
+        len0_to_write = bytearray(struct.pack("i", len0))
+        val1 = bytearray(struct.pack("i", self._int_test_value))
+        len1 = len(val1)
+        len1_to_write = bytearray(struct.pack("i", len1))
+        val2 = bytearray('TestString'.encode())
+        len2 = len(val2)
+        len2_to_write = bytearray(struct.pack("i", len2))
+        self._multiple_cols_file_stream.write(len0_to_write)
+        self._multiple_cols_file_stream.write(val0)
+        self._multiple_cols_file_stream.write(len1_to_write)
+        self._multiple_cols_file_stream.write(val1)
+        self._multiple_cols_file_stream.write(len2_to_write)
+        self._multiple_cols_file_stream.write(val2)
+        self._multiple_cols_file_stream.close()
+        self._multiple_cols_file_stream = open("E:\\temp.txt", "rb")
+
+        # Readers:
+        self._bool_reader = ServiceBufferFileStreamReader(self._bool_file_stream)
+        self._float_reader = ServiceBufferFileStreamReader(self._float_file_stream)
+        self._multiple_cols_reader = ServiceBufferFileStreamReader(self._multiple_cols_file_stream)
+
+    def tearDown(self):
+        pass
+
+    def test_read_bool(self):
+        test_file_offset = 0
+        test_row_id = 1
+        test_columns_info = []
+
+        col = DbColumn()
+        col.data_type_name = datatypes.DATATYPE_BOOL
+        test_columns_info.append(col)
+
+        res = self._bool_reader.read_row(test_file_offset, test_row_id, test_columns_info)
+        self.assertEqual(self._bool_test_value, res[0].raw_object)
+
+    def test_read_float(self):
+        test_file_offset = 0
+        test_row_id = 1
+        test_columns_info = []
+
+        col = DbColumn()
+        col.data_type_name = datatypes.DATATYPE_REAL
+        test_columns_info.append(col)
+
+        res = self._float_reader.read_row(test_file_offset, test_row_id, test_columns_info)
+        self.assertEqual(self._float_test_value, res[0].raw_object)
+
+    def test_read_multiple_cols(self):
+        test_file_offset = 0
+        test_row_id = 1
+        test_columns_info = []
+
+        col0 = DbColumn()
+        col0.data_type_name = datatypes.DATATYPE_REAL
+        col1 = DbColumn()
+        col1.data_type_name = datatypes.DATATYPE_INTEGER
+        col2 = DbColumn()
+        col2.data_type_name = datatypes.DATATYPE_TEXT
+
+        test_columns_info.append(col0)
+        test_columns_info.append(col1)
+        test_columns_info.append(col2)
+
+        res = self._multiple_cols_reader.read_row(test_file_offset, test_row_id, test_columns_info)
+        self.assertEqual(123.45600128173828, res[0].raw_object)
+        self.assertEqual(123456, res[1].raw_object)
+        self.assertEqual('TestString', res[2].raw_object)
