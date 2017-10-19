515058614d7d0fdf8e32c8932c9a0f165d0466cc
liusun <liusun@microsoft.com>
2017-10-19 11:56:15 -0700

    fix Mac/Linux test failure on test_write_long

--- pgsqltoolsservice/converters/bytes_converter.py
+++ pgsqltoolsservice/converters/bytes_converter.py
@@ -14,6 +14,12 @@ from pgsqltoolsservice.parsers import datatypes
 from psycopg2.extras import NumericRange, DateTimeRange, DateTimeTZRange, DateRange
 
 
+def _get_range_data_type_bound(value):
+    lower_bound = "[" if value.lower_inc else "("
+    upper_bound = "]" if value.upper_inc else ")"
+    return [lower_bound, upper_bound]
+
+
 def convert_bool(value: bool):
     return bytearray(struct.pack("?", value))
 
@@ -27,17 +33,20 @@ def convert_double(value: float):
 
 
 def convert_short(value: int):
-    """ range of smallint in pg is the same with short in c, although python type is int, but need to pack in short format """
+    """ range of smallint in pg is the same with short in c,
+    although python type is int, but need to pack the value in short format """
     return bytearray(struct.pack("h", value))
 
 
-def convert_long(value: int):
-    """ range of integer in pg is the same with long in c, although python type is int, but need to pack in long format """
-    return bytearray(struct.pack("l", value))
+def convert_int(value: int):
+    """ range of integer in pg is the same with int and long in c,
+    we pack the value in int format """
+    return bytearray(struct.pack("i", value))
 
 
 def convert_long_long(value: int):
-    """ range of bigint in pg is the same with long long in c, although python type is int, but need to pack in long long format """
+    """ range of bigint in pg is the same with long long in c,
+    although python type is int, but need to pack the value in long long format """
     return bytearray(struct.pack("q", value))
 
 
@@ -93,33 +102,29 @@ def convert_list(value: list):
 
 def convert_numericrange(value: NumericRange):
     """ Serialize NumericRange object in "[lower,upper)" format before convert to bytearray """
-    lower_bound = "[" if value.lower_inc else "("
-    upper_bound = "]" if value.upper_inc else ")"
-    formatted_value_str = lower_bound + str(int(value.lower)) + "," + str(int(value.upper)) + upper_bound
+    bound = _get_range_data_type_bound(value)
+    formatted_value_str = bound[0] + str(int(value.lower)) + "," + str(int(value.upper)) + bound[1]
     return bytearray(formatted_value_str.encode())
 
 
 def convert_datetimerange(value: DateTimeRange):
     """ Serialize DateTimeRange object in "[lower,upper)" format before convert to bytearray """
-    lower_bound = "[" if value.lower_inc else "("
-    upper_bound = "]" if value.upper_inc else ")"
-    formatted_value_str = lower_bound + str(value.lower.isoformat()) + "," + str(value.upper.isoformat()) + upper_bound
+    bound = _get_range_data_type_bound(value)
+    formatted_value_str = bound[0] + str(value.lower.isoformat()) + "," + str(value.upper.isoformat()) + bound[1]
     return bytearray(formatted_value_str.encode())
 
 
 def convert_datetimetzrange(value: DateTimeTZRange):
     """ Serialize DateTimeTZRange object in "[lower,upper)" format before convert to bytearray """
-    lower_bound = "[" if value.lower_inc else "("
-    upper_bound = "]" if value.upper_inc else ")"
-    formatted_value_str = lower_bound + str(value.lower.isoformat()) + "," + str(value.upper.isoformat()) + upper_bound
+    bound = _get_range_data_type_bound(value)
+    formatted_value_str = bound[0] + str(value.lower.isoformat()) + "," + str(value.upper.isoformat()) + bound[1]
     return bytearray(formatted_value_str.encode())
 
 
 def convert_daterange(value: DateRange):
     """ Serialize DateRange object in "[lower,upper)" format before convert to bytearray """
-    lower_bound = "[" if value.lower_inc else "("
-    upper_bound = "]" if value.upper_inc else ")"
-    formatted_value_str = lower_bound + str(value.lower.isoformat()) + "," + str(value.upper.isoformat()) + upper_bound
+    bound = _get_range_data_type_bound(value)
+    formatted_value_str = bound[0] + str(value.lower.isoformat()) + "," + str(value.upper.isoformat()) + bound[1]
     return bytearray(formatted_value_str.encode())
 
 
@@ -128,7 +133,7 @@ DATATYPE_WRITER_MAP = {
     datatypes.DATATYPE_REAL: convert_float,
     datatypes.DATATYPE_DOUBLE: convert_double,
     datatypes.DATATYPE_SMALLINT: convert_short,
-    datatypes.DATATYPE_INTEGER: convert_long,
+    datatypes.DATATYPE_INTEGER: convert_int,
     datatypes.DATATYPE_BIGINT: convert_long_long,
     datatypes.DATATYPE_NUMERIC: convert_decimal,
     datatypes.DATATYPE_CHAR: convert_char,
@@ -142,7 +147,7 @@ DATATYPE_WRITER_MAP = {
     datatypes.DATATYPE_INTERVAL: convert_timedelta,
     datatypes.DATATYPE_UUID: convert_uuid,
     datatypes.DATATYPE_SMALLSERIAL: convert_short,
-    datatypes.DATATYPE_SERIAL: convert_long,
+    datatypes.DATATYPE_SERIAL: convert_int,
     datatypes.DATATYPE_BIGSERIAL: convert_long_long,
     datatypes.DATATYPE_MONEY: convert_str,
     datatypes.DATATYPE_BYTEA: convert_memoryview,
