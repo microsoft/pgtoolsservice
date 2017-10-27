1e40e243a27571e403aa074740d0741bdba43920
praveenbarli <prbarl@microsoft.com>
2017-10-26 20:00:12 -0700

    added database change handler in ConnectionService

--- pgsqltoolsservice/connection/contracts/__init__.py
+++ pgsqltoolsservice/connection/contracts/__init__.py
@@ -5,6 +5,7 @@
 
 from pgsqltoolsservice.connection.contracts.cancellation_request import CANCEL_CONNECT_REQUEST, CancelConnectParams
 from pgsqltoolsservice.connection.contracts.connect_request import CONNECT_REQUEST, ConnectRequestParams
+from pgsqltoolsservice.connection.contracts.change_database_request import CHANGE_DATABASE_REQUEST, ChangeDatabaseRequestParams
 from pgsqltoolsservice.connection.contracts.disconnect_request import DISCONNECT_REQUEST, DisconnectRequestParams
 from pgsqltoolsservice.connection.contracts.connection_complete_notification import (
     CONNECTION_COMPLETE_METHOD,
@@ -21,6 +22,7 @@ from pgsqltoolsservice.connection.contracts.list_databases import (
 __all__ = [
     'CANCEL_CONNECT_REQUEST', 'CancelConnectParams',
     'CONNECT_REQUEST', 'ConnectRequestParams',
+    'CHANGE_DATABASE_REQUEST', 'ChangeDatabaseRequestParams',
     'DISCONNECT_REQUEST', 'DisconnectRequestParams',
     'CONNECTION_COMPLETE_METHOD', 'ConnectionCompleteParams',
     'ConnectionDetails', 'ConnectionSummary', 'ConnectionType', 'ServerInfo',
