cd5d4564fdf043fc13d327f10fc296f37d95abd0
unknown <akamath46297>
2017-11-17 19:25:42 +0000

    Removed methods

--- pgsqltoolsservice/query/data_storage/save_as_excel_writer_factory.py
+++ pgsqltoolsservice/query/data_storage/save_as_excel_writer_factory.py
@@ -5,19 +5,13 @@
 
 import io
 
-from pgsqltoolsservice.query.data_storage import FileStreamFactory, SaveAsExcelWriter, ServiceBufferFileStreamReader
+from pgsqltoolsservice.query.data_storage import FileStreamFactory, SaveAsExcelWriter
 
 
 class SaveAsExcelFileStreamFactory(FileStreamFactory):
 
     def __init__(self, params) -> None:
         FileStreamFactory.__init__(self, params)
-
-    def create_file(self) -> str:
-        raise NotImplementedError()
-
-    def get_reader(self, file_name: str):
-        return ServiceBufferFileStreamReader(io.open(file_name, 'rb'))
 
     def get_writer(self, file_name: str):
         return SaveAsExcelWriter(io.open(file_name, 'w'), self._params)
--- pgsqltoolsservice/query/data_storage/save_as_json_file_stream_factory.py
+++ pgsqltoolsservice/query/data_storage/save_as_json_file_stream_factory.py
@@ -5,19 +5,13 @@
 
 import io
 
-from pgsqltoolsservice.query.data_storage import FileStreamFactory, SaveAsJsonWriter, ServiceBufferFileStreamReader
+from pgsqltoolsservice.query.data_storage import FileStreamFactory, SaveAsJsonWriter
 
 
 class SaveAsJsonFileStreamFactory(FileStreamFactory):
 
     def __init__(self, params) -> None:
         FileStreamFactory.__init__(self, params)
-
-    def create_file(self) -> str:
-        raise NotImplementedError()
-
-    def get_reader(self, file_name: str):
-        return ServiceBufferFileStreamReader(io.open(file_name, 'rb'))
 
     def get_writer(self, file_name: str):
         return SaveAsJsonWriter(io.open(file_name, 'w'), self._params)
