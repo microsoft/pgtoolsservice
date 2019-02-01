95720d9f9bfc3db8f6f6422281b2b4f870e8a5a5
unknown <akamath46297>
2019-02-01 04:22:07 +0000

    Added a hack to resolve the issue of default database in azure data studio

--- pgsqltoolsservice/parsers/__init__.py
+++ pgsqltoolsservice/parsers/__init__.py
@@ -5,8 +5,10 @@
 
 import pgsqltoolsservice.parsers.datatypes
 import pgsqltoolsservice.parsers.datatype_parsers  # noqa
+import pgsqltoolsservice.parsers.owner_uri_parser  # noqa
 
 __all__ = [
     'datatypes',
-    'datatype_parsers'
+    'datatype_parsers',
+    'owner_uri_parser'
 ]
