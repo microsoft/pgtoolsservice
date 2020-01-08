53bee565eca3f9a7004dee6dfc6ed6dc33d7b5a4
swjain23 <36710109+swjain23@users.noreply.github.com>
2020-01-08 10:16:44 -0800

    Remove tlc changes

--- setup.py
+++ setup.py
@@ -1,5 +1,4 @@
 from cx_Freeze import setup, Executable
-import os
 
 # Dependencies are automatically detected, but it might need
 # fine tuning.
@@ -11,9 +10,6 @@ base = 'Console'
 executables = [
     Executable('pgsqltoolsservice/pgtoolsservice_main.py', base=base)
 ]
-
-os.environ['TCL_LIBRARY'] = r'C:\Users\swjain\AppData\Local\Programs\Python\Python36-32\tcl\tcl8.6'
-os.environ['TK_LIBRARY'] = r'C:\Users\swjain\AppData\Local\Programs\Python\Python36-32\tcl\tk8.6'
 
 setup(name='PostgreSQL Tools Service',
       version='0.1.0',
