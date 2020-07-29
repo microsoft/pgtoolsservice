a1986237facdb99d9e9aabcf245430ef29da134c
swjain23 <36710109+swjain23@users.noreply.github.com>
2020-07-29 11:13:04 -0700

    Fix query editor connect/disconnect (#252)
    * Fix query editor connect/disconnect
    
    When connection is closed by server, the qurery execution fails but we were trying to set autocommit to true on a closed connection which throws an error.
    Now check the status of the connection. This will ensure that the status is set to executed and user can connect/disconnect properly.
    
    * Remove whitespace
    
    * Add a comment to explain closed property
    
    * Update the comment

--- pgsqltoolsservice/query/query.py
+++ pgsqltoolsservice/query/query.py
@@ -141,7 +141,10 @@ class Query:
 
                 batch.execute(connection)
         finally:
-            connection.autocommit = current_auto_commit_status
+            # 0 if the connection is open, nonzero if it is closed or broken.
+            # We can only set autocommit when the connection is open.
+            if (connection.closed == 0):
+                connection.autocommit = current_auto_commit_status
             self._execution_state = ExecutionState.EXECUTED
 
     def get_subset(self, batch_index: int, start_index: int, end_index: int):
