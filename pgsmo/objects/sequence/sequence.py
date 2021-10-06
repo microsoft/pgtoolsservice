7a7fb3b075d7d6a43ea677b8e861e0c38863200c
Daeun Yim <69321306+DaeunYim@users.noreply.github.com>
2021-10-06 10:20:34 -0700

    Add PG Restore/Dump for version 13 and Fix sequence in column and sequence object (#295)
    * pgrestore and dump for version 13
    
    * fix column object and add properties
    
    * fix sequence object and column object
    
    * Broken column object fixed
    
    * style fixed

--- pgsmo/objects/sequence/sequence.py
+++ pgsmo/objects/sequence/sequence.py
@@ -3,9 +3,9 @@
 # Licensed under the MIT License. See License.txt in the project root for license information.
 # --------------------------------------------------------------------------------------------
 
-from typing import List
+from typing import Optional, List, Dict
 
-from smo.common.node_object import NodeObject
+from smo.common.node_object import NodeObject, NodeLazyPropertyCollection, NodeCollection
 from smo.common.scripting_mixins import ScriptableCreate, ScriptableDelete, ScriptableUpdate
 from pgsmo.objects.server import server as s    # noqa
 import smo.utils.templating as templating
@@ -37,12 +37,40 @@ class Sequence(NodeObject, ScriptableCreate, ScriptableDelete, ScriptableUpdate)
         return seq
 
     def __init__(self, server: 's.Server', parent: NodeObject, name: str):
-        NodeObject.__init__(self, server, parent, name)
+        self._server = server
+        self._parent: Optional['NodeObject'] = parent
+        self._name: str = name
+        self._oid: Optional[int] = None
+        self._is_system: bool = False
+
+        self._child_collections: Dict[str, NodeCollection] = {}
+        self._property_collections: List[NodeLazyPropertyCollection] = []
+        # Use _column_property_generator instead of _property_generator
+        self._full_properties: NodeLazyPropertyCollection = self._register_property_collection(self._sequence_property_generator)
+
         ScriptableCreate.__init__(self, self._template_root(server), self._macro_root(), server.version)
         ScriptableDelete.__init__(self, self._template_root(server), self._macro_root(), server.version)
         ScriptableUpdate.__init__(self, self._template_root(server), self._macro_root(), server.version)
         self._schema: str = None
         self._scid: int = None
+        self._def: dict = None
+
+    def _sequence_property_generator(self):
+        template_root = self._template_root(self._server)
+
+        # Setup the parameters for the query
+        template_vars = self.template_vars
+
+        # Render and execute the template
+        sql = templating.render_template(
+            templating.get_template_path(template_root, 'properties.sql', self._server.version),
+            self._macro_root(),
+            **template_vars
+        )
+        cols, rows = self._server.connection.execute_dict(sql)
+
+        if len(rows) > 0:
+            return rows[0]
 
     # PROPERTIES ###########################################################
     @property
@@ -54,7 +82,6 @@ class Sequence(NodeObject, ScriptableCreate, ScriptableDelete, ScriptableUpdate)
         return self._scid
 
     # -FULL OBJECT PROPERTIES ##############################################
-
     @property
     def cycled(self):
         return self._full_properties.get("cycled", "")
