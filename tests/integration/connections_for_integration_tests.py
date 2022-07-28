feeee5ac910f554b598df3eed4b9cb6236c8b123
rishky-msft <rishabhkumar@microsoft.com>
2022-07-28 11:24:18 +0530

    Refactoring integration test setup to support multiple providers integration tests

---
+++ tests/integration/connections_for_integration_tests.py
@@ -0,0 +1,59 @@
+# --------------------------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for license information.
+# --------------------------------------------------------------------------------------------
+
+"""Module containing the logic to set up database connection for integration tests"""
+
+from abc import abstractclassmethod
+from typing import List
+import psycopg2
+
+
+class IConnection():
+
+    """
+    Opens the connection for the list of connection details dict provided
+
+    :params connection_details_list: list of connection details dict
+    """
+    @abstractclassmethod
+    def open_connections(cls, connection_details_list):
+        pass
+
+    """
+    Fetch the list of connections opened
+    """
+    @abstractclassmethod
+    def get_connections(cls):
+        pass
+
+    """
+    Get the server version of the connection at index
+
+    :params index: index of connection to be fetched
+    """
+    @abstractclassmethod
+    def get_connection_server_version(cls, index):
+        pass
+
+
+class PostgreSQLConnection:
+
+    _connections: List[psycopg2.extensions.connection] = []
+
+    @classmethod
+    def open_connections(cls, connection_details_list):
+        cls._connections = []
+        for config_dict in connection_details_list:
+            connection = psycopg2.connect(**config_dict)
+            cls._connections.append(connection)
+            connection.autocommit = True
+
+    @classmethod
+    def get_connections(cls):
+        return cls._connections
+
+    @classmethod
+    def get_connection_server_version(cls, index):
+        return cls._connections[index].server_version
