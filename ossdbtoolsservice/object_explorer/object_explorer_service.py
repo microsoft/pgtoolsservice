a5bbdbb8063e4013feede90256e0c5173bd42a6d
rishky-msft <102506628+rishky-msft@users.noreply.github.com>
2022-09-22 20:58:40 +0530

    Object Explorer Session : Invalidating child node cache entries on re… (#327)
    * Object Explorer Session : Invalidating child node cache entries on refresh of parent node
    
    * Renaming

--- ossdbtoolsservice/object_explorer/object_explorer_service.py
+++ ossdbtoolsservice/object_explorer/object_explorer_service.py
@@ -371,6 +371,9 @@ class ObjectExplorerService(object):
                 match = route.match(path)
                 if match is not None:
                     # We have a match!
+                    if is_refresh:
+                        # Invalidate cache for all paths with current path as suffix
+                        self._remove_path_from_session_cache(session, path)
                     target_nodes = target.get_nodes(is_refresh, path, session, match.groupdict())
                     session.cache[path] = target_nodes
                     return target_nodes
@@ -383,3 +386,7 @@ class ObjectExplorerService(object):
         else:
             # Return the results of a previous request for the same path
             return session.cache[path]
+    
+    def _remove_path_from_session_cache(self, session: ObjectExplorerSession, path: str) -> None:
+        updated_cache = {key:value for key, value in session.cache.items() if key.startswith(path) == False}
+        session.cache = updated_cache
