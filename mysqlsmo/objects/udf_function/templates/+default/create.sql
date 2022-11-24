a45b30001ea9f51b2880ccb166084e5cb1cd518e
rishky-msft <rishabhkumar@microsoft.com>
2022-11-24 16:29:14 +0530

    Fixing mysql routing logic for identifiers name detection

--- mysqlsmo/objects/udf_function/templates/+default/create.sql
+++ mysqlsmo/objects/udf_function/templates/+default/create.sql
@@ -2,4 +2,4 @@
 # Copyright (c) Microsoft Corporation. All rights reserved.
 # Licensed under the MIT License. See License.txt in the project root for license information.
 #}
-CREATE FUNCTION IF NOT EXISTS {{fn_name}} RETURNS {{ret}} SONAME {{dl | string_convert}};
+CREATE FUNCTION IF NOT EXISTS `{{fn_name}}` RETURNS {{ret}} SONAME {{dl | string_convert}};
