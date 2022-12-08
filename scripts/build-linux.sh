a9c8db877d1fd70a1d70395fa47cbf1db4aeb22a
nitishsharma25 <nitishsharma@microsoft.com>
2022-12-08 12:48:19 +0530

    Updated build scripts

--- scripts/build-linux.sh
+++ scripts/build-linux.sh
@@ -13,6 +13,17 @@ PYTHONPATH=
 pip3 install -r requirements.txt
 python3 setup.py build
 
+# Compress mysqltoolsservice folder
+cd $pwd/build
+if [ "$1" = ubuntu22 ]
+then
+    tar -cvzf mysqltoolsservice-ubuntu22-x64.tar.gz mysqltoolsservice
+else
+    tar -cvzf mysqltoolsservice-linux-x64.tar.gz mysqltoolsservice
+fi
+
 # Restore the old PYTHONPATH and move back to the original directory
 cd $pwd
 PYTHONPATH=$old_pythonpath
+
+# Pass ubuntu22 param to build for Ubuntu22 else build will be for other linux distribution
