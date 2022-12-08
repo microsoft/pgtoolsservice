3087d7e76a5f30b35b03a751fcfd95fc0fb008a9
nitishsharma25 <nitishsharma@microsoft.com>
2022-12-08 12:51:26 +0530

    Updated setup.py

--- setup.py
+++ setup.py
@@ -2,9 +2,14 @@ from cx_Freeze import setup, Executable
 
 # Dependencies are automatically detected, but it might need
 # fine tuning.
-buildOptions = dict(packages=['asyncio', 'jinja2', 'pymysql', 'inflection', 'sqlparse',
-                              'prompt_toolkit', 'xlsxwriter', 'nose', 'parameterized', 'coverage', 'autopep8', 'flake8', 'debugpy', '_pydev_runfiles'],
-                    excludes=[], include_files=[], replace_paths=[("*", "")])
+buildOptions = dict(
+    build_exe = "build/mysqltoolsservice",
+    packages = ['asyncio', 'jinja2', 'pymysql', 'inflection', 'sqlparse',
+                'prompt_toolkit', 'xlsxwriter', 'nose', 'parameterized', 'coverage', 'autopep8', 'flake8', 'debugpy', '_pydev_runfiles'],
+    excludes = [],
+    include_files = [],
+    replace_paths = [("*", "")]
+)
 
 base = 'Console'
 
@@ -12,8 +17,10 @@ executables = [
     Executable('ossdbtoolsservice/ossdbtoolsservice_main.py', base=base)
 ]
 
-setup(name='OSS Databases Tools Service',
-      version='1.6.2',
-      description='Carbon data protocol server implementation for PostgreSQL',
-      options=dict(build_exe=buildOptions),
-      executables=executables)
+setup(
+    name = 'OSS Databases Tools Service',
+    version = '1.6.2',
+    description = 'Carbon data protocol server implementation for PostgreSQL',
+    options = dict(build_exe=buildOptions),
+    executables = executables
+)
