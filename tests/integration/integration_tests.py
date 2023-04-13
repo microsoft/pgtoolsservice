20aa85788ea06dad8d3815205877cd26d82614b5
Daeun Yim <69321306+DaeunYim@users.noreply.github.com>
2023-04-13 12:56:57 -0700

    fix style issue

--- tests/integration/integration_tests.py
+++ tests/integration/integration_tests.py
@@ -131,7 +131,7 @@ class _ConnectionManager:
         if not os.path.exists(config_path):
             raise RuntimeError(f'No test config file found at {config_path}')
 
-        with open(config_path,  encoding='utf-8-sig') as f:
+        with open(config_path, encoding='utf-8-sig') as f:
             json_content = f.read()
             config_list = json.loads(json_content)
 
