773f7d8fe2577a97ab2d80af42bc0fff6e40f4b6
Daeun Yim <69321306+DaeunYim@users.noreply.github.com>
2023-04-29 10:19:32 -0700

    Changing package to support all platforms (#391) (#392)
    * Changing package to support all platforms (#391)
    
    * Update initialize_request.py
    
    * Change packaging to support all platforms
    
    * Dev/daeunyim/release prep m1 (#393)
    
    * Update initialize_request.py
    
    * Change packaging to support all platforms
    
    * Adding macros and fix an exception handling
    
    * Make lietrals.py work in windows as well
    
    * Make any mac (m1 or intel) to use psycopg2-binary

--- ossdbtoolsservice/language/completion/packages/pgliterals/main.py
+++ ossdbtoolsservice/language/completion/packages/pgliterals/main.py
@@ -5,9 +5,24 @@
 
 import os
 import json
+import sys
+
+# Determine if the application is running as a packaged binary
+is_packaged = getattr(sys, 'frozen', False)
 
 root = os.path.dirname(__file__)
-literal_file = os.path.join(root, 'pgliterals.json')
+
+if is_packaged:
+    # In packaged mode, use the path relative to the binary's directory
+    literal_file = os.path.join(getattr(sys, '_MEIPASS', '.'), 'language', 'completion', 'packages', 'pgliterals', 'pgliterals.json')
+else:
+    # In development mode, use the path relative to this file's directory
+    literal_file = os.path.join(root, 'pgliterals.json')
+
+# Check if the path points to a directory
+if os.path.isdir(literal_file):
+    print(f"Path '{literal_file}' points to a directory, which is unexpected.")
+    sys.exit(1)
 
 with open(literal_file) as f:
     literals = json.load(f)
