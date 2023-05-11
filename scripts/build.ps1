1f1ebd16ca1e76f1d5d19e1cf77b37ebbf0a4326
Daeun Yim <69321306+DaeunYim@users.noreply.github.com>
2023-05-11 11:10:16 -0700

    Build script and pipeline yml files (#406)
    * Fix broken language service
    
    * Modify build script and add CI pipelines
    
    * update build and codesign yml files to target crossbuildoutsPlatform
    
    * remove unnecessary condition
    
    * change setting to database system
    
    * fix some mal configurations
    
    * change the version to 1.6.0
    
    * change artifact feed name
    
    * try replacing with internal guids
    
    * revert the change
    
    * revert dyanmic loading per newly added libraries
    
    * revert unneccessary change
    
    * make the latest downloaded
    
    * revert to specific version name
    
    * fix style issue and trigger pipeline

--- scripts/build.ps1
+++ scripts/build.ps1
@@ -1,3 +1,8 @@
+# --------------------------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for license information.
+# --------------------------------------------------------------------------------------------
+
 # Save the current directory and the script's directory, since build must be run from the project root
 $curloc = $pwd
 $scriptloc = $PSScriptRoot
@@ -9,7 +14,16 @@ $oldPythonpath=$Env:PYTHONPATH
 Set-Location $scriptloc/..
 $Env:PYTHONPATH = ""
 pip3 install -r requirements.txt
-python setup.py build
+pyinstaller ossdbtoolsservice_main.spec
+
+# create folder pgsqltoolsservice in dist folder
+New-Item -ItemType Directory -Force -Path ".\dist\pgsqltoolsservice"
+
+# Move the contents in the dist folder to pgsqltoolsservice folder
+Get-ChildItem -Path ".\dist" | Where-Object { !$_.PSIsContainer } | Move-Item -Destination ".\dist\pgsqltoolsservice"
+
+# copy pg_exe folder to pgsqltoolsservice
+Copy-Item ".\ossdbtoolsservice\pg_exes" ".\dist\pgsqltoolsservice\pg_exes" -Recurse
 
 # Restore the old PYTHONPATH and move back to the original directory
 Set-Location $curloc
