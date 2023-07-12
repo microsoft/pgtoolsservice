8e73f86a8c602ffaa24d29528b12f71e6a357569
Daeun Yim <69321306+DaeunYim@users.noreply.github.com>
2023-07-12 11:13:06 -0700

    Update common.py

--- ossdbtoolsservice/workspace/contracts/common.py
+++ ossdbtoolsservice/workspace/contracts/common.py
@@ -98,10 +98,6 @@ class TextDocumentPosition(Serializable):
     def ignore_extra_attributes(cls):
         return True
 
-    @classmethod
-    def ignore_extra_attributes(cls):
-        return True
-
 
 class Location(Serializable):
     """
