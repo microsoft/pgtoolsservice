8c95354489a1b11824cd5dd7fb95fc083237cf60
Naia Scott <nasc@microsoft.com>
2023-07-18 15:51:44 -0400

    style check all platforms

--- scripts/flake8.sh
+++ scripts/flake8.sh
@@ -5,6 +5,17 @@ then
   exit 1
 fi
 
+# Function to check if a command is available
+command_exists() {
+  command -v "$1" >/dev/null 2>&1
+}
+
+# Install flake8 if it's missing
+if ! command_exists flake8; then
+  echo "flake8 not found. Installing flake8..."
+  pip3 install flake8
+fi
+
 flake8 --max-line-length=160 --ignore W605,W503,W504 --builtins psycopg,ossdbtoolsservice,View ossdbtoolsservice
 flake8 --max-line-length=160 --ignore W605,W503,W504 --builtins psycopg,ossdbtoolsservice,View tests
 flake8 --max-line-length=160 --ignore W605,W503,W504 --builtins psycopg,ossdbtoolsservice,View pgsmo
