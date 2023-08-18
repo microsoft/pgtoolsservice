6e3eb47089f7575c0ac2da57cf7df39ae02722e6
samir-puranik <samir.puranik@gmail.com>
2023-08-18 12:27:59 -0700

    updated constraint_fk templating files

--- pgsmo/objects/table_objects/constraint_fk/+default/get_cols.sql
+++ pgsmo/objects/table_objects/constraint_fk/+default/get_cols.sql
@@ -1,7 +1,13 @@
+{#
+ # pgAdmin 4 - PostgreSQL Tools
+ #
+ # Copyright (C) 2013 - 2017, The pgAdmin Development Team
+ # This software is released under the PostgreSQL Licence
+ #}
 {% for n in range(colcnt|int) %}
 {% if loop.index != 1 %}
-UNION SELECT  pg_get_indexdef({{ cid|string }}, {{ loop.index|string }}, true) AS column
+UNION SELECT  pg_catalog.pg_get_indexdef({{ cid|string }}, {{ loop.index|string }}, true) AS column
 {% else %}
-SELECT  pg_get_indexdef({{ cid|string }} , {{ loop.index|string }} , true) AS column
+SELECT  pg_catalog.pg_get_indexdef({{ cid|string }} , {{ loop.index|string }} , true) AS column
 {% endif %}
 {% endfor %}
