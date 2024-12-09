f8a996040e6e57cd7e7db8a8f955caaff92ebc38
Matt McFarland <mmcfarland@microsoft.com>
2024-12-09 16:55:43 -0500

    Remote test instance

--- website_fabric_client/src/utils/constants.ts
+++ website_fabric_client/src/utils/constants.ts
@@ -1,2 +1,3 @@
 // The URL where PGTS over HTTP is running
-export const BASE_HTTP_URL = "http://localhost:8080";
+export const BASE_HTTP_URL =
+  "https://e1f60d5b07a3.southcentralus.azurecontainer.io:8443";
