8b38f2eb5d103ccbd485ce82c6a77cb22b46800d
Rob Emanuele <rdemanuele@gmail.com>
2025-02-04 22:10:30 -0500

    Add async runner to service provider

--- ossdbtoolsservice/hosting/service_provider.py
+++ ossdbtoolsservice/hosting/service_provider.py
@@ -7,15 +7,27 @@ from logging import Logger
 from typing import Optional
 
 from ossdbtoolsservice.hosting import JSONRPCServer
+from ossdbtoolsservice.utils.async_runner import AsyncRunner
 
 
 class ServiceProvider:
-    def __init__(self, json_rpc_server: JSONRPCServer, services: dict, provider: str, logger: Optional[Logger] = None):
+    def __init__(
+        self,
+        json_rpc_server: JSONRPCServer,
+        services: dict,
+        provider: str,
+        logger: Optional[Logger] = None,
+        async_runner: AsyncRunner | None = None,
+    ):
         self._is_initialized = False
         self._logger = logger
         self._server = json_rpc_server
         self._provider_name = provider
-        self._services = {service_name: service_class() for (service_name, service_class) in services.items()}
+        self._services = {
+            service_name: service_class()
+            for (service_name, service_class) in services.items()
+        }
+        self._async_runner = async_runner
 
     # PROPERTIES ###########################################################
 
@@ -30,6 +42,10 @@ class ServiceProvider:
     @property
     def provider(self) -> str:
         return self._provider_name
+    
+    @property
+    def async_runner(self) -> AsyncRunner | None:
+        return self._async_runner
 
     def __getitem__(self, item: str):
         """
@@ -39,7 +55,9 @@ class ServiceProvider:
         :return: The requested service
         """
         if not self._is_initialized:
-            raise RuntimeError('Service provider must be initialized before retrieving services')
+            raise RuntimeError(
+                "Service provider must be initialized before retrieving services"
+            )
 
         return self._services[item]
 
@@ -51,7 +69,7 @@ class ServiceProvider:
         :raises RuntimeError: Service provider has been initialized already
         """
         if self._is_initialized:
-            raise RuntimeError('Service provider cannot be initialized more than once')
+            raise RuntimeError("Service provider cannot be initialized more than once")
 
         # Set initialized true before calling register, as otherwise services can't look each
         # other up. This is important since services can register callbacks with each other
@@ -59,3 +77,10 @@ class ServiceProvider:
 
         for service_key in self._services:
             self._services[service_key].register(self)
+
+    def shutdown(self) -> None:
+        """
+        Shutdown logic for services
+        """
+        if self._async_runner is not None:
+            self._async_runner.shutdown()
