6b162e4a4f5ea58e2d92e46d30e02de1c0dc150e
Rob Emanuele <rdemanuele@gmail.com>
2025-02-06 13:41:39 -0500

    Remove version from artifact names

--- scripts/get-artifact-name.sh
+++ scripts/get-artifact-name.sh
@@ -1,27 +1,25 @@
 #!/bin/bash
-# Arguments: $1 = os, $2 = version
+
+# This script is used to get the artifact name based on the operating system.
+# This runs as part of the GitHub Actions workflow.
+
+# Arguments: $1 = os
 os="$1"
-version="$2"
 artifact=""
 
 if [[ "$os" == "windows-latest" ]]; then
-    artifact="pgsqltoolsservice-win-x64-${version}.zip"
-elif [[ "$os" == *"macos"* ]]; then
-    # For macOS assume we check architecture at runtime
-    if [[ "$os" == "macos-13" || "$os" == "macos-latest" ]]; then
-        if [[ "$(uname -m)" == "arm64" ]]; then
-            artifact="pgsqltoolsservice-osx-arm64-${version}.tar.gz"
-        else
-            artifact="pgsqltoolsservice-osx-${version}.tar.gz"
-        fi
-    fi
-elif [[ "$os" == *"ubuntu"* ]]; then
-    # For Linux, check architecture
-    if [[ "$(uname -m)" == "aarch64" ]]; then
-        artifact="pgsqltoolsservice-linux-arm64-${version}.tar.gz"
-    else
-        artifact="pgsqltoolsservice-linux-x64-${version}.tar.gz"
-    fi
+    artifact="pgsqltoolsservice-win-x64.zip"
+elif [[ "$os" == "macos-13" ]]; then
+    artifact="pgsqltoolsservice-osx.tar.gz"
+elif [[ "$os" == "macos-latest" ]]; then
+    artifact="pgsqltoolsservice-osx-arm64.tar.gz"
+elif [[ "$os" == "ubuntu-latest" ]]; then
+    artifact="pgsqltoolsservice-linux-x64.tar.gz"
+elif [[ "$os" == "ubuntu-22.04-arm" ]]; then
+    artifact="pgsqltoolsservice-linux-arm64.tar.gz"
+else
+    echo "Unknown operating system: $os"
+    exit 1
 fi
 
 echo "${artifact}"
