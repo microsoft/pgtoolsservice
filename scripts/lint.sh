a462f96c76c4b2bddf48e017f6bd8c59db99475a
Rob Emanuele <rdemanuele@gmail.com>
2025-02-18 11:33:49 -0500

    Add mypy to lint script.
    Only runs over ossdbtoolsservice and tests_v2. Does not follow imports, which would end up checking other projects. This is to scope the linting to where new development will happen.
    
    Skips checking ossdbtoolsservice.language.completion. This module was particularly tricky to fix with typing. Could be tackled in future, but for now allow this to continue to be untyped.

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -18,4 +18,8 @@ if [ "${BASH_SOURCE[0]}" = "${0}" ]; then
     echo "Running ruff check..."
     ruff check ossdbtoolsservice tests tests_v2 pgsmo
 
+    echo "Running mypy check..."
+    # Don't follow imports; we are not checking other legacy packages.
+    mypy ossdbtoolsservice tests_v2
+
 fi
